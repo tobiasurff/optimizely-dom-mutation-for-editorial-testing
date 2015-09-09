808812a4fbfb81e473314310c00ea4375b57905d
Tobias Urff <tobias@urff.at>
2015-09-09 22:20:56 +0200

    Changed elementsToDecorate array for naming consistency

--- project_js/code.js
+++ project_js/code.js
@@ -1,5 +1,5 @@
 window.optimizelyEditorial = {
-    elementsToRender: [],
+    elementsToDecorate: [],
     waitForElement: function(identifier, selector, fn) {
         // If Mutation Observers are available
         if (window.MutationObserver || window.WebKitMutationObserver) {
@@ -28,8 +28,8 @@ window.optimizelyEditorial = {
                             element.ready = true;
                             
                             // Add element to array so that it can be picked up from within variation code
-                            window.optimizelyEditorial.elementsToRender[identifier] = window.optimizelyEditorial.elementsToRender[identifier] || [];
-                            window.optimizelyEditorial.elementsToRender[identifier].push(element);
+                            window.optimizelyEditorial.elementsToDecorate[identifier] = window.optimizelyEditorial.elementsToDecorate[identifier] || [];
+                            window.optimizelyEditorial.elementsToDecorate[identifier].push(element);
                            
                             // Invoke the callback with the element
                             listener.fn.call(element, element);
@@ -54,7 +54,7 @@ window.optimizelyEditorial = {
         // Loop through items
         for (var i = 0; i < items.length; i++) {
             // Trigger callback every time an element matching the selector is added to the page
-            // Every element will be pushed to the window.optimizelyEditorial.elementsToRender array once so that your experiment code can pick it up and decorate accordingly
+            // Every element will be pushed to the window.optimizelyEditorial.elementsToDecorate array once so that your experiment code can pick it up and decorate accordingly
             window.optimizelyEditorial.waitForElement(items[i], 'article:has(a[href*="' + items[i] + '"])',
                 function() {
                     callback.call();
@@ -66,9 +66,9 @@ window.optimizelyEditorial = {
         if (!identifier) {
             return false;
         }
-        // Get the last element added to the window.optimizelyEditorial.elementsToRender array to make sure each element gets treated only once, even if the experiment activates mutliple times on the page
-        if (window.optimizelyEditorial.elementsToRender[identifier].length > 0) {
-            var elem = window.optimizelyEditorial.elementsToRender[identifier].pop();
+        // Get the last element added to the window.optimizelyEditorial.elementsToDecorate array to make sure each element gets treated only once, even if the experiment activates mutliple times on the page
+        if (window.optimizelyEditorial.elementsToDecorate[identifier].length > 0) {
+            var elem = window.optimizelyEditorial.elementsToDecorate[identifier].pop();
         } else {
             return false;
         }
