db9c2142e26183039777f049c403b95267f21478
Tobias Urff <tobias@urff.at>
2015-09-09 20:18:17 +0200

    Update code.js

--- variation_js/code.js
+++ variation_js/code.js
@@ -1,5 +1,5 @@
 /* _optimizely_evaluate=force */
-optimizelyEditorial.decorateItem("12345", {{
+optimizelyEditorial.decorateItem("12345", {
     headline: "This is the variation headline",
     teaser_image: "http://www.example.com/some/path/test.jpg"
 });
