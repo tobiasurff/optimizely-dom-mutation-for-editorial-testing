1d68f7c5668c8820f6b2a68e8bee6a0349b7546e
Toby Urff <toby@optimizely.com>
2015-09-17 16:39:35 +0200

    Added Project JS

--- project_js/code.js
+++ project_js/code.js
@@ -1,12 +1,16 @@
+/* Merchandise testing */
 window.optimizelyEditorial = {
     elementsToDecorate: [],
-    waitForElement: function(identifier, selector, fn) {
+    waitForElement: function(selector, fn) {
+
         // If Mutation Observers are available
         if (window.MutationObserver || window.WebKitMutationObserver) {
+
             var listeners = [],
                 doc = window.document,
                 MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
                 observer;
+
             // Store the selector and callback to be monitored
             listeners.push({
                 selector: selector,
@@ -26,60 +30,86 @@ window.optimizelyEditorial = {
                         // same element more than once
                         if (!element.ready) {
                             element.ready = true;
-                            
+
                             // Add element to array so that it can be picked up from within variation code
                             window.optimizelyEditorial.elementsToDecorate[identifier] = window.optimizelyEditorial.elementsToDecorate[identifier] || [];
                             window.optimizelyEditorial.elementsToDecorate[identifier].push(element);
-                           
+
                             // Invoke the callback with the element
                             listener.fn.call(element, element);
                         }
                     }
                 }
             }
+
             if (!observer) {
+
                 // Watch for changes in the document
                 observer = new MutationObserver(check);
                 observer.observe(doc.documentElement, {
                     childList: true,
                     subtree: true
                 });
+
             }
+
             // Check if the element is currently in the DOM
             check();
+
         }
 
     },
     itemOnPage: function(items, callback) {
-        // Loop through items
+
         for (var i = 0; i < items.length; i++) {
-            // Trigger callback every time an element matching the selector is added to the page
-            // Every element will be pushed to the window.optimizelyEditorial.elementsToDecorate array once so that your experiment code can pick it up and decorate accordingly
-            window.optimizelyEditorial.waitForElement(items[i], 'article:has(a[href*="' + items[i] + '"])',
+            window.optimizelyEditorial.waitForElement(items[i], 'article:has(a[href*="-' + items[i] + '.html"]):not(.content)',
                 function() {
                     callback.call();
                 });
+
+            // If it's the article page itself
+            if (document.location.href.indexOf('-' + items[i] + '.html') > -1) {
+                window.optimizelyEditorial.waitForElement(items[i], 'article.content .story-intro',
+                    function() {
+                        callback.call();
+                    });
+            }
         }
+
+
     },
     decorateItem: function(identifier, data) {
-        // Make sure mandatory information (like the identifier) is in the data object
-        if (!identifier) {
+
+        if (!data.new_headline) {
             return false;
         }
+
+
         // Get the last element added to the window.optimizelyEditorial.elementsToDecorate array to make sure each element gets treated only once, even if the experiment activates mutliple times on the page
         if (window.optimizelyEditorial.elementsToDecorate[identifier].length > 0) {
             var elem = window.optimizelyEditorial.elementsToDecorate[identifier].pop();
         } else {
             return false;
         }
-        // Check if treatment is available for e.g. headlines, teaser images etc. and apply changes
-        if (data.headline) {
+
+        if (data.new_headline) {
             $(elem)
-                .find('h1').text(data.headline);
+                .find('h1:not(:has(a)),h1 a,h2:not(:has(a)), h2 a, h3:not(:has(a)), h3 a, h4:not(:has(a)), h4 a').text(data.new_headline);
         }
-        if (data.teaser_image) {
+        if (data.new_subhead) {
             $(elem)
-                .find('img').attr('src', data.teaser_image);
+                .find('.subhead, .tease p').text(data.new_subhead);
         }
+        if (data.new_image) {
+            $(elem)
+                .find('figure img').attr('src', data.new_image);
+            $(elem)
+                .find('figure:not(:has(img)) a').attr('style', 'background-image: url(' + data.new_image + ')');
+        }
+        if (data.new_imagesource) {
+            $(elem)
+                .find('.art figcaption p').text(data.new_imagesource);
+        }
+
     }
 };
