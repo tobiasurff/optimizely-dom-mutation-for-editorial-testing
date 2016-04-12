376589542e6c4db897819ce570cb194b3424f1ac
Tobias Urff <turff@ams-turff1-4.local>
2016-04-12 15:02:35 +0200

    Editorial Testing Tool template ready.

--- variation_js/code.js
+++ variation_js/code.js
@@ -1,5 +1,8 @@
+/* _optimizely_evaluate=editor_only */
+optimizelyEditorial.itemOnPage(["/edition/406/ressort/companies-markets/article/the-bonus-battle-at-volkswagen"], function(){}); 
+/* _optimizely_evaluate=end_editor_only */ 
 /* _optimizely_evaluate=force */
-optimizelyEditorial.decorateItem("/edition/405/ressort/finance/article/man-in-the-middle", {
+optimizelyEditorial.decorateItem("/edition/406/ressort/companies-markets/article/the-bonus-battle-at-volkswagen", {
     overline: "Test Overline",
     headline: "This is the variation headline",
     teaser: "Test teaser text",
